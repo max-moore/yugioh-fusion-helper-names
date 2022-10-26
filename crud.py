2629f209796d3202f7c272c9236141922971ab2b
max-moore <maxwell.moorebillings@gmail.com>
2022-10-26 14:46:04 -0700

    commented crud.py and main.py

--- crud.py
+++ crud.py
@@ -5,25 +5,34 @@ from sqlalchemy.orm import Session
 
 import models, schemas
 
+# function that takes a card id as input and returns the card object from the scraped_card_list table
 def get_card(db: Session, card_id: int):
     return db.query(models.Card).filter(models.Card.card_id == card_id).first()
 
+# function that takes a card id as input and returns the fusion object from the fusion_list table
 def get_fusion(db: Session, fusion_id: int):
     return db.query(models.Fusion).filter(models.Fusion.fusion_id == fusion_id).first()
 
+# function that takes a list of up to 5 card ids, and returns all possible fusions as fusion ids from the fusion_list table
 def fusion_calc(db: Session, user_hand: List[int]):
     fusion_list = []
-    
+    # hand_count selects the current card in hand
     hand_count = 1
-
+    # while we're not selecting the last card in our hand...
     while hand_count < len(user_hand):
+        # our first material is our currently selected card
         material_one = user_hand[hand_count - 1]
+        # and for each subsequent card in our hand...
         for card in user_hand[hand_count:]:
+            # search for fusions!
             material_two = card
             fusion = db.query(models.Fusion.fusion_id).filter(
                 and_(models.Fusion.material_one == material_one, models.Fusion.material_two == material_two)).first()
+            # if we find a fusion and it isn't already in our list...
             if fusion and fusion not in fusion_list:
+                # then add it to our list
                 fusion_list.append(fusion)
+        # select the next card in hand for our first material
         hand_count += 1
-
+    # return our full list of fusions
     return fusion_list
--- yfh_dbscrape.py
+++ yfh_dbscrape.py
@@ -8,63 +8,83 @@ from bs4 import BeautifulSoup
 import psycopg2
 from psycopg2 import OperationalError
 
+# function to scrape a full list of cards from the wiki
 def card_list_scrape() -> list:
     wiki_URL = "https://yugipedia.com"
     card_list_URL = "/wiki/List_of_Yu-Gi-Oh!_Forbidden_Memories_cards"
 
+    # initializing BeautifulSoup object using the card list URL, and finding the first table
     soup = BeautifulSoup(requests.get(f"{wiki_URL}{card_list_URL}").content, "html.parser")
     table = soup.find('table', attrs={'class':'wikitable'})
 
+    # navigating the table to select the card list
     card_list = table.find_next('tbody').find_all('tr')[1:]
     card_table_list = []
 
+    # for each card in the list...
     for card in card_list:
+        # navigate to the name of the card and format it by lower casing it, removing punctuation, and replacing spaces with underscores
         card_name = card.find_next('td').find_next('td')
         card_name_formatted = card_name.a.string.lower().translate(str.maketrans('', '', string.punctuation)).replace(' ', '_')
-
+        # find the card's type
         card_type = card.find_next('td').find_next('td').find_next('td').a.string
-
+        # find the URL for the individual card page
         card_url = card_name.a['href']
-
+        # if the card is a monster...
         if card_type == 'Monster':
+            # navigate to the individual card page and select the table to find its attack points
             soup = BeautifulSoup(requests.get(f"{wiki_URL}{card_url}").content, "html.parser")
             table = soup.find('table', attrs={'class':'innertable'})
 
             atk_points = table.find_next('tr').find_next('tr').find_next('tr').find_next('td').string.split('/')[0].strip()
         else:
+            # if it isn't a monster, set the atk_points to -1
             atk_points = -1
-
+        # append the card with its attack points to our formatted list of cards
         card_table_list.append([card_name_formatted, atk_points])
+        # print statement for testing purposes
         print(card_name_formatted)
-   
+   # return our full card list
     return(card_table_list)
 
+# function to create a list of all possible fusions that can be made using a particular card by scraping from the wiki
 def fusion_material_scrape(card_name) -> list:
     fusion_list = []
-
+    # for each of the 4 pages on the wiki that list fusions... 
     for fusion_page in fusions_pages:
+        # create a BeautifulSoup object and select all tables (each table is a fusion)
         soup = BeautifulSoup(requests.get(fusion_page).content, "html.parser")
         tables = soup.find_all('table', attrs={'class':'wikitable'})
 
+        # for each table (fusion)...
         for table in tables:
+            # create a fusion_name variable by formatting the name of the fusion from the wiki
             fusion_name = table.find_previous('div').a.text.lower().replace(' (fmr)', '')
             fusion_name = fusion_name.translate(str.maketrans('', '', string.punctuation)).replace(' ', '_')
+            # selecting a section for each possible way to make a particular fusion
             materials = table.find_all('tr')[1:]
+            # for each material within each section...
             for material_option in materials:
+                # make a formatted list of materials that can be used as material one and two
                 material_one_options = material_option.find_next('td').find_all('li')
                 material_two_options = material_option.find_next('td').find_next('td').find_all('li')
 
                 material_one_options = [card1.a.string.lower().translate(str.maketrans('', '', string.punctuation)).replace(' ', '_') for card1 in material_one_options]
                 material_two_options = [card2.a.string.lower().translate(str.maketrans('', '', string.punctuation)).replace(' ', '_') for card2 in material_two_options]
 
+                # for each material 1...
                 for card in material_one_options:
+                    # if material 1 is the same card as our parameter for the function...
                     if card_name == card:
+                        # append an item to our fusion list, a list containing a list of material 2 options and the name of our fusion
                         fusion_list.append([material_two_options, fusion_name])
-    
+                # for each material 2...
                 for card in material_two_options:
+                    # if material 2 is the same card as our parameter for the function...
                     if card_name == card:
+                        # append an item to our fusion list, a list containing a list of material 1 options and the name of our fusion
                         fusion_list.append([material_one_options, fusion_name])
-
+    # return the fusion list
     return(fusion_list)
 
 def create_db_connection(db_name, db_user, db_password, db_host, db_port):
@@ -112,28 +132,38 @@ def fusion_table_creator(cursor):
 
 # Used to populate fusion table
 def fusion_table_populate(card_name_list, cursor):
+    # unique id for identifying fusions
     fusion_id_count = 1
 
+    # for each card in our full list of cards scraped from the wiki
     for card in card_name_list:
+        # grab a list of possible fusions using our fusion_material_scrape function
         fusion_list = fusion_material_scrape(card)
 
+        # grab the id of our currently selected card
         select_material_one_id = f"SELECT card_id FROM scraped_card_list WHERE card_name='{card}'"
         cursor.execute(select_material_one_id)
         material_one_id = cursor.fetchall()[0][0]
 
+        # for each fusion in our list of possible fusions for our selected card...
         for fusion in fusion_list:
+            # grab the id of the possible fusion
             select_result_id = f"SELECT card_id FROM scraped_card_list WHERE card_name='{fusion[1]}'"
             cursor.execute(select_result_id)
             result_id = cursor.fetchall()[0][0]
 
+            # for each material that can be used as a second material for the possible fusion...
             for material_two in fusion[0]:
+                # grab the id of the second material
                 select_material_two_id = f"SELECT card_id FROM scraped_card_list WHERE card_name='{material_two}'"
                 cursor.execute(select_material_two_id)
                 material_two_id = cursor.fetchall()[0][0]
 
+                # insert a new record with our unique fusion id, the ids for materials used for the fusion, and the resulting fusion id
                 insert_fusion_sql = f"INSERT INTO fusion_list (fusion_id, material_one, material_two, result) VALUES ({fusion_id_count}, {material_one_id}, {material_two_id}, {result_id})"                
                 cursor.execute(insert_fusion_sql)
 
+                # prints for testing purposes and displaying progress while adding records to our database
                 print(f"entry added - {card}, {material_two}, {fusion[1]}")
                 print(f"fusion count {fusion_id_count}")
                 fusion_id_count += 1
